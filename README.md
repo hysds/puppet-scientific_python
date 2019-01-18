a7b3f0f6b4b6ead950b9e6afdecc4a2937e80f79
Pan Ziyue <random.rrr3@gmail.com>
2019-01-18 14:27:44 +0800

    added azure build chain for puppet-scientific_python

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # scientific_python
 
-Puppet module to install all software needed to use 
-python in a scientific environment.
-
+Puppet module to install all software needed to use python in a scientific environment.
 
 ## Prerequisites
+
 Create a base CentOS7 image as described [here](https://github.com/hysds/hysds-framework/wiki/Puppet-Automation#create-a-base-centos-7-image-for-installation-of-all-hysds-component-instances).
 
-
 ## Installation
+
 As _root_ run:
-```
-bash < <(curl -skL https://github.com/hysds/puppet-scientific_python/raw/master/install.sh)
+
+```sh
+bash < <(curl -skL https://github.com/earthobservatory/puppet-scientific_python/raw/azure-beta1/install.sh)
 ```
--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 mods_dir=/etc/puppet/modules
 cd $mods_dir
 
+###############################################################################
+# IMPORTANT: please edit this branch name to the version of the Azure         #
+#            adaptation that you want to install on every Puppet module       #
+###############################################################################
+git_branch="azure-beta1"
+
+
 ##########################################
 # need to be root
 ##########################################
@@ -54,13 +61,14 @@ fi
 # export scientific_python puppet module
 ##########################################
 
-git_loc="${git_url}/hysds/puppet-scientific_python"
+git_loc="${git_url}/earthobservatory/puppet-scientific_python"
+
 mod_dir=$mods_dir/scientific_python
 site_pp=$mod_dir/site.pp
 
 # check that module is here; if not, export it
 if [ ! -d $mod_dir ]; then
-  $git_cmd clone $git_loc $mod_dir
+  $git_cmd clone -b $git_branch --single-branch $git_loc $mod_dir
 fi
 
 
